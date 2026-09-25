e575613ac2b3123dd55cef242e7347c116c1d316
anxmhd <anass.ymhd@gmail.com>
2026-09-25 23:09:02 +0000

    Updates: - BankAccount.java - CourseNumbersArrays.java - DogShelter.java - ManageStudent.java

--- src/main/java/samplearrays/ManageStudent.java
+++ src/main/java/samplearrays/ManageStudent.java
@@ -95,7 +95,7 @@ public class ManageStudent {
     // 10) Expandable Array: return a new array with one more slot and append student
     public static Student[] appendStudent(Student[] students, Student newStudent) {
         Student[] newStudents = new Student[students.length + 1];
-        for(int i = 0; i < students.length; i++) {
+        for (int i = 0; i < students.length; i++) {
             newStudents[i] = new Student(students[i].getId(), students[i].getName(), students[i].getAge(), students[i].getGrade());
         }
         newStudents[students.length] = newStudent;
@@ -114,50 +114,82 @@ public class ManageStudent {
         students[4] = new Student(4, "Elise", 18, 18);
 
         // Print all
-        System.out.println("== All Students ==");
+        System.out.println("\n=== All Students ===");
         for (Student s : students) System.out.println(s);
-        System.out.println("Total created: " + Student.getNumStudent());
+
+        System.out.println("\n=== Total created ===");
+        System.out.println(Student.getNumStudent());
 
         // 2) Oldest
         Student oldest = findOldest(students);
-        System.out.println("Oldest : " + oldest);
+        System.out.println("\n=== Oldest ===");
+        System.out.println(oldest);
 
         // 3) Count adults
         int countAdult = countAdults(students);
-        System.out.println("Adults count : " + countAdult);
+        System.out.println("\n=== Adults count ===");
+        System.out.println(countAdult);
 
         // 4) Average grade
         double avg = averageGrade(students);
-        System.out.println("Average : " + avg);
+        System.out.println("\n=== Average ===");
+        System.out.println(avg);
 
         // 5) Find by name
         Student foundStudent = findStudentByName(students, "Elise");
-        System.out.println("Found Student : " + foundStudent);
+        System.out.println("\n=== Found Student ====");
+        System.out.println(foundStudent);
 
         // 6) Sort by grade desc
         // sort function
-        System.out.println("\n== Sorted by grade (desc) ==");
+        System.out.println("\n=== Sorted by grade (desc) ===");
         for (Student s : students) System.out.println(s);
 
         // 7) High achievers >= 15
-        System.out.println("\nHigh achievers:");
+        System.out.println("\n=== High achievers ===");
         printHighAchievers(students);
 
         // 8) Update grade by id
         boolean updated = updateGrade(students, 1, 15);
         // function
         System.out.println("\nUpdated id=4? " + updated);
-        System.out.println("Found Student Dina : " + findStudentByName(students, "Dina"));
+        System.out.println("Found Student Dina " + findStudentByName(students, "Dina"));
 
         // 9) Duplicate names
         boolean duplicates = hasDuplicateNames(students);
-        System.out.println("Has Duplicate names : " + duplicates);
+        System.out.println("\n=== Has Duplicate names ===");
+        System.out.println(duplicates);
 
         // 10) Append new student
         Student newStudent = new Student(5, "Anass", 19, 20);
         Student[] newStudents = appendStudent(students, newStudent);
-        System.out.println("=== New Students : ===");
+        System.out.println("\n=== New Students ===");
         for (Student s : newStudents) System.out.println(s);
+
+        // 11) Top Students in 2 classes
+        Student[][] school = new Student[2][3];
+        school[0][0] = new Student(0, "John", 19, 11);
+        school[0][1] = new Student(1, "Alice", 18, 19);
+        school[0][2] = new Student(2, "Alex", 20, 15);
+        school[1][0] = new Student(3, "Bob", 19, 17);
+        school[1][1] = new Student(4, "Elise", 18, 18);
+        school[1][2] = new Student(5, "Mohamed", 21, 12);
+
+        Student[] topStudents = new Student[2];
+        for(int i = 0; i < school.length; i++) {
+            Student topStudent = school[i][0];
+            for(int j = 0; j < school[0].length; j++) {
+                if(topStudent.getGrade() < school[i][j].getGrade()) topStudent = school[i][j];
+            }
+            topStudents[i] = topStudent;
+        }
+
+        System.out.println("\n=== Top Student Class 1 ===");
+        System.out.println(topStudents[0]);
+
+        System.out.println("\n=== Top Student Class 2 ===");
+        System.out.println(topStudents[1]);
+
     }
 }
 
